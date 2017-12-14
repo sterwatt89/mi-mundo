8b9c3b9f26b43ee5514224f30670c376c1b990cd
Esther Watt <missterwatt@gmail.com>
2017-12-14 13:22:17 +0100

    14/12

---
+++ 12 -13-2017/js/noseque.js
@@ -0,0 +1,28 @@
+
+
+
+
+var semaforo:true
+
+
+
+
+
+
+
+
+
+
+
+ 1) Implementa un semáforo
+        Se pide implementar el código necesario para mostrar:
+            - Una imagen
+            - Un botón
+        Y el siguiente comportamiento:
+            -Cuando el usuario hace click en el botón...
+                -> El botón alterna su aspecto (estilo y contenido)
+                -> La imagen se cambia por otra
+        
+        Dificultad 1: Con una variable global y dos <img>
+        Dificultad 2: Con una variable global y con una <img>
+        Dificultad 3 (funciones puras): sin variables globales y una <img>
---
+++ javaScript/js/variable.js
@@ -0,0 +1,63 @@
+// declarar variable
+
+
+var personme;
+
+
+var dianaci;
+var mesnaci;
+var añonaci;
+var añostengo;
+var cero;
+var mesactual;
+var diactual;
+
+var diasvividos;
+
+// Asignar valor a la variable
+
+
+personme = "Esther";
+dianaci = 06;
+mesnaci= 04;
+añonaci = 1989;
+añoactual= 2017;
+añostengo = añoactual - añonaci;
+cero = "0";
+mesactual = 12;
+diactual = 5;
+diasvividos = 
+
+
+
+
+if (dianaci < 10) { dianaci = 
+
+
+
+
+ dianaci }
+
+if (mesnaci < 10) { mesnaci = cero + mesnaci }
+
+
+
+if (mesactual < mesnaci)  { añostengo = añostengo - 1  }
+
+
+if (mesactual == mesnaci && diactual < dianaci) { añostengo = añostengo - 1  }
+
+/* en una condicion, en el enunciado el igual es doble y el y es && */// 
+
+
+/* +++++++++++++++++++++++++ OUTPUT++++++++++++++++++++++*/// 
+
+
+console.log("Mi nombre es  " + personme ); 
+console.log("Naci el "  + dianaci + " del "  + mesnaci + " de " + añonaci); 
+console.log( "Por tanto tengo " + añostengo + " años ");
+
+
+console.log( "Asi que he vivido " + diasvividos + " dias ");
+
+
